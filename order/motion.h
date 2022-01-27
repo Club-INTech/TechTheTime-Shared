44f21aac5e6bad8161efcb80f05f4c4fbd0c0614
StarQTius <dussautoirp@gmail.com>
2022-01-27 16:08:22 +0100

    Add orders for movement & PID settings

--- order/motion.h
+++ order/motion.h
@@ -1,10 +1,22 @@
 #pragma once
 
+#include <cstddef>
+
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
 
-void motion_set_translation_setpoint(uint32_t);
+typedef uint16_t Shared_Tick;
+typedef double Shared_PID_K;
+
+void Motion_Set_Forward_Translation_Setpoint(Shared_Tick);
+void Motion_Set_Backward_Translation_Setpoint(Shared_Tick);
+void Motion_Set_Clockwise_Rotation_Setpoint(Shared_Tick);
+void Motion_Set_Counterclockwise_Rotation_Setpoint(Shared_Tick);
+void Motion_Set_Translation_PID(Shared_PID_K, Shared_PID_K, Shared_PID_K);
+void Motion_Set_Rotation_PID(Shared_PID_K, Shared_PID_K, Shared_PID_K);
+void Motion_Set_Left_PID(Shared_PID_K, Shared_PID_K, Shared_PID_K);
+void Motion_Set_Right_PID(Shared_PID_K, Shared_PID_K, Shared_PID_K);
 
 #ifdef __cplusplus
 } //extern "C"
