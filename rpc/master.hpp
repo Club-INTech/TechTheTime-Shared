30421addc4485e582b09dccadd8a47e6519d7259
zbascoulard <zbascoulard@outlook.fr>
2022-04-22 00:32:37 +0200

    dynamixel updtated within the shell

--- rpc/master.hpp
+++ rpc/master.hpp
@@ -5,6 +5,7 @@
 
 #include <order/hub.h>
 #include <order/motion.h>
+#include <order/dxl.h>
 
 namespace rpc {
 namespace master {
@@ -15,15 +16,17 @@ constexpr k2o::keyring keyring{
     Motion_Set_Backward_Translation_Setpoint,
     Motion_Set_Clockwise_Rotation_Setpoint,
     Motion_Set_Counterclockwise_Rotation_Setpoint,
-	Motion_Start_Joystick,
+	  Motion_Start_Joystick,
     Motion_Set_Joystick,
     Motion_Set_Translation_PID,
     Motion_Set_Rotation_PID,
     Motion_Set_Left_PID,
     Motion_Set_Right_PID,
-	Motion_Release,
-  Motion_Set_Free_Movement,
-	Hub_Set_Mode
+	  Motion_Release,
+    Motion_Set_Free_Movement,
+	  Hub_Set_Mode,
+    DXL_Position,
+    DXL_Position_Angle
 	>,
 	upd::little_endian,
 	upd::two_complement
